da030b6f1ee23f157e04dc6cab97ec4d52b6c0b3
SadSquashy <104904126+SadSquashy@users.noreply.github.com>
2024-06-20 02:35:39 +0300

    Create payload.ps1

---
+++ payload.ps1
@@ -0,0 +1,19 @@
+$hookUrl = "https://discord.com/api/webhooks/1250354460669251644/Ul6yBVr6V1AHHomvLgWrbCnofLaheVPtAfMOhmB2owABtpg4OaNarlzq8ddFySU4ag9l"
+
+$content = @"
+Balls
+"@
+
+$payload = [PSCustomObject]@{
+    content = $content
+}
+
+$params = @{
+    Uri = $hookUrl
+    Method = 'POST'
+    Body = ($Payload | ConvertTo-Json)
+    ContentType = 'application/json'
+}
+        
+Invoke-RestMethod @params
+
